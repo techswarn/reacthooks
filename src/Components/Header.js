f58b6b03d789e0b6760a49129efe82c0f8f66885
Swarn Chandrahasa Suvarna <diya-swarns@corp.systechmlr.net>
2021-06-19 14:13:36 +0530

    par-2-accessing-context

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,20 +2,26 @@ import React, { Component } from "react";
 import { ThemeContext } from "./../Contexts/ThemeContext";
 
 class Header extends Component {
-  static contextType = ThemeContext;
   render() {
     // console.log(this.context);
-    const { isLightTheme, light, dark } = this.context;
-    const theme = isLightTheme ? light : dark;
+
     return (
-      <nav style={{ background: theme.ui, color: theme.syntax }}>
-        <h1>Context app</h1>
-        <ul>
-          <li>Home</li>
-          <li>Contact</li>
-          <li>About</li>
-        </ul>
-      </nav>
+      <ThemeContext.Consumer>
+        {(context) => {
+          const { isLightTheme, light, dark } = context;
+          const theme = isLightTheme ? light : dark;
+          return (
+            <nav style={{ background: theme.ui, color: theme.syntax }}>
+              <h1>Context app</h1>
+              <ul>
+                <li>Home</li>
+                <li>Contact</li>
+                <li>About</li>
+              </ul>
+            </nav>
+          );
+        }}
+      </ThemeContext.Consumer>
     );
   }
 }
